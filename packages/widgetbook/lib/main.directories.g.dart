f57e50a8c441ad88dee1879eb117b814ebe64365
Niels <niels@iconica.nl>
2024-01-26 10:25:02 +0100

    changes

--- packages/widgetbook/lib/main.directories.g.dart
+++ packages/widgetbook/lib/main.directories.g.dart
@@ -9,31 +9,6 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:timeline_widgetbook/main.dart' as _i2;
 import 'package:widgetbook/widgetbook.dart' as _i1;
 
-final directories = <_i1.WidgetbookNode>[
-  _i1.WidgetbookFolder(
-    name: 'screens',
-    children: [
-      _i1.WidgetbookLeafComponent(
-        name: 'TimelinePostScreen',
-        useCase: _i1.WidgetbookUseCase(
-          name: 'Timeline post screen 2',
-          builder: _i2.postScreenUseCase,
-          designLink:
-              'https://www.figma.com/file/PRJoVXQ5aOjAICfkQdAq2A/Iconica-User-Stories?type=design&node-id=34-2763&mode=design&t=W72P3tkEascAKDCk-4',
-        ),
-      ),
-      _i1.WidgetbookLeafComponent(
-        name: 'TimelineScreen',
-        useCase: _i1.WidgetbookUseCase(
-          name: 'Timeline screen 2',
-          builder: _i2.timelineUseCase,
-          designLink:
-              'https://www.figma.com/file/PRJoVXQ5aOjAICfkQdAq2A/Iconica-User-Stories?type=design&node-id=34-2763&mode=design&t=W72P3tkEascAKDCk-4',
-        ),
-      ),
-    ],
-  )
-];
+final directories = <_i1.WidgetbookNode>[];
