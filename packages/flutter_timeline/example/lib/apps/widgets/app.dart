604c082827e91ce43b666144baf421e29e6aa28c
Vick Top <vick@iconica.nl>
2024-02-13 16:16:39 +0100

    fix(ci): run dart format and flutter analyze

--- packages/flutter_timeline/example/lib/apps/widgets/app.dart
+++ packages/flutter_timeline/example/lib/apps/widgets/app.dart
@@ -32,7 +32,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var timelineService = TimelineService(postService: LocalTimelinePostService());
+  var timelineService =
+      TimelineService(postService: LocalTimelinePostService());
   var timelineOptions = options;
 
   @override
